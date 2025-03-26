315b21894e424cc801f72d6306fb9eb80530a1ef
Marcin Paszkowski <041985marcin@gmail.com>
2025-03-26 10:24:00 +0100

    🔥 Usunięto plik z tokenem z historii

--- push_with_token.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Skrypt do pushu z tokenem – UZUPEŁNIJ TOKEN!
-:: Token generujesz na: https://github.com/settings/tokens
-
-set TOKEN=GH_TOKEN_TUTAJ
-set USER=Marcinpp1
-
-git remote set-url origin https://%USER%:%TOKEN%@github.com/Marcinpp1/auto-evolution-gpt.git
-git push origin main
